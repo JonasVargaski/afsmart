db3d7398d35191c35320884a03aec4d63954c0b6
Jonas <jonasvargaski@hotmail.com>
2020-07-29 21:23:44 -0300

    sync with sockey

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,58 @@
 
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <Wire.h>
 #include <PortExpander_I2C.h>
-#include <ArduinoOTA.h>
 #include <SocketIoClient.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <Wire.h>
 
-PortExpander_I2C pciOUT(0x27);
-PortExpander_I2C pciIN(0x26);
+PortExpander_I2C peOUT(0x27);
+PortExpander_I2C peIN(0x26);
 SocketIoClient socket;
 
 int interruptPCI_in = 5;
 
-const char *ssid = "Vargaski";        //  SSID da sua rede
-const char *password = "12345678";    // senha da sua rede
-const char *server = "192.168.0.104"; // endereço servidor
+const char *ssid = "Vargaski";
+const char *password = "12345678";
 
-IPAddress ip(192, 168, 0, 115);    // ip atribuido ao módulo
-IPAddress gateway(192, 168, 0, 1); // base ip da sua rede
-IPAddress subnet(255, 255, 255, 0);
+const char *server = "192.168.0.104";
+const int serverPort = 3000;
 
-void event(const char *payload, size_t length)
+String mac = WiFi.macAddress();
+String pwd = "123456";
+
+void handler(const char *message, size_t length)
 {
-  Serial.printf("got message: %s\n", payload);
-}
+  DynamicJsonDocument json(1024);
+  deserializeJson(json, message);
 
-void socket_Connected(const char *payload, size_t length)
-{
-  Serial.println("Socket.IO connected!");
+  peOUT.digitalWrite(3, !json["0"]); // RL1
+  peOUT.digitalWrite(6, !json["1"]); // RL2
+  peOUT.digitalWrite(5, !json["2"]); // RL3
+  peOUT.digitalWrite(4, !json["3"]); // RL4
+  peOUT.digitalWrite(0, !json["4"]); // RL5
+  peOUT.digitalWrite(1, !json["5"]); // RL6
+  peOUT.digitalWrite(2, !json["6"]); // RL7
+  peOUT.digitalWrite(7, !json["7"]); // LED STATUS
 }
 
 void setup(void)
 {
   Wire.begin(0, 2);
-  pciOUT.init();
-  pciIN.init();
+  peOUT.init();
+  peIN.init();
 
   for (int i = 0; i < 8; i++)
   {
-    pciIN.pinMode(i, INPUT);
+    peIN.pinMode(i, INPUT);
   }
   pinMode(interruptPCI_in, INPUT);
 
   Serial.begin(115200);
-  Serial.println("Booting");
+  Serial.println("\r\n\r\nBooting...");
 
-  WiFi.config(ip, gateway, subnet);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
+
   while (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
     Serial.println("Connection Failed! Rebooting...");
@@ -56,56 +60,45 @@ void setup(void)
     ESP.restart();
   }
 
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
-    Serial.println("Start updating " + type);
-  });
-
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR)
-      Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR)
-      Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR)
-      Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR)
-      Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR)
-      Serial.println("End Failed");
-  });
-
-  socket.on("ping", event);
-  socket.on("connect", socket_Connected);
-  // socket.setReconnectInterval(5000);
-  // socket.enableHeartbeat(15000, 3000, 2);
-  socket.begin(server, 3000, "/socket.io/?EIO=3&transport=websocket");
-  // ArduinoOTA.begin();
+  socket.begin(server, serverPort);
+  socket.on("server->device", handler);
 }
 
 void loop(void)
 {
-  //  ArduinoOTA.handle();
   socket.loop();
 
   if (!digitalRead(interruptPCI_in))
   {
-    pciOUT.digitalWrite(3, pciIN.digitalRead(0)); // Ligado ao CN1_4 pciOUT Acessoria Controla RL1
-    pciOUT.digitalWrite(6, pciIN.digitalRead(1)); // Ligado ao CN1_3 pciOUT Acessoria Controla RL2
-    pciOUT.digitalWrite(5, pciIN.digitalRead(2)); // Ligado ao CN1_2 pciOUT Acessoria Controla RL3
-    pciOUT.digitalWrite(4, pciIN.digitalRead(3)); // Ligado ao CN1_1 pciOUT Acessoria Controla RL4
-    pciOUT.digitalWrite(0, pciIN.digitalRead(4)); // Ligado ao CN2_4 pciOUT Acessoria Controla RL5
-    pciOUT.digitalWrite(1, pciIN.digitalRead(5)); // Ligado ao CN2_3 pciOUT Acessoria Controla RL6
-    pciOUT.digitalWrite(2, pciIN.digitalRead(6)); // Ligado ao CN2_2 pciOUT Acessoria Controla RL7
-    pciOUT.digitalWrite(7, pciIN.digitalRead(7)); // Ligado ao CN2_1 pciOUT Acessoria Controla LED STATUS
+    DynamicJsonDocument json(1024);
+
+    json["mac"] = mac;
+
+    JsonObject inputs = json.createNestedObject("inputs");
+
+    inputs["1"] = !peIN.digitalRead(0);
+    inputs["2"] = !peIN.digitalRead(1);
+    inputs["3"] = !peIN.digitalRead(2);
+    inputs["4"] = !peIN.digitalRead(3);
+    inputs["5"] = !peIN.digitalRead(4);
+    inputs["6"] = !peIN.digitalRead(5);
+    inputs["7"] = !peIN.digitalRead(6);
+    inputs["8"] = !peIN.digitalRead(7);
+
+    JsonObject outputs = json.createNestedObject("outputs");
+
+    outputs["1"] = !peOUT.digitalRead(3);
+    outputs["2"] = !peOUT.digitalRead(6);
+    outputs["3"] = !peOUT.digitalRead(5);
+    outputs["4"] = !peOUT.digitalRead(4);
+    outputs["5"] = !peOUT.digitalRead(0);
+    outputs["6"] = !peOUT.digitalRead(1);
+    outputs["7"] = !peOUT.digitalRead(2);
+    outputs["8"] = !peOUT.digitalRead(7);
+
+    char serializedJson[500];
+
+    serializeJson(json, serializedJson);
+    socket.emit("device->server", serializedJson);
   }
 }
